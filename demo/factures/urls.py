140f7a0c406a9acfb8c6597490453a8567f43695
Moussa Bamba <moussabulls1@gmail.com>
2024-10-24 11:25:29 +0200

    Tests & Class View

--- demo/factures/urls.py
+++ demo/factures/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from .views import FactureCreateView, FactureListView, CategorieCreateView, ClientCreateView, ClientList
 
 urlpatterns = [
-    path('factures/', views.liste_factures, name='liste_factures'),
-    path('factures/creer/', views.creer_facture, name='creer_facture'),
+    path('factures/', FactureListView.as_view(), name='liste_factures'),
+    path('factures/creer/', FactureCreateView.as_view(), name='creer_facture'),
     path('factures/modifier/<int:id>/', views.modifier_facture, name='modifier_facture'),
     path('factures/supprimer/<int:id>/', views.supprimer_facture, name='supprimer_facture'),
-    path('ajouter-categorie/', views.ajouter_categorie, name='ajouter_categorie'),
-    path('ajouter-client/', views.ajouter_client, name='ajouter_client'),
-    path('clients/', views.liste_clients, name='liste_clients'),
+    path('ajouter-categorie/', CategorieCreateView.as_view(), name='ajouter_categorie'),
+    path('ajouter-client/', ClientCreateView.as_view(), name='ajouter_client'),
+    path('clients/', ClientList.as_view(), name='liste_clients'),
 ]
