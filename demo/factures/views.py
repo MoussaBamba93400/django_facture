8672e5ae83cd7c00664ccbf1933b627850c6f5c9
Moussa Bamba <moussabulls1@gmail.com>
2024-10-24 11:25:12 +0200

    Tests & Class View

--- demo/factures/views.py
+++ demo/factures/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Facture, Categorie, Client
 from .forms import FactureForm, CategorieForm, ClientForm
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
 
-def creer_facture(request):
-    if request.method == 'POST':
-        print('here')
-        form = FactureForm(request.POST)
-        if form.is_valid():
-            facture = form.save(commit=False)
-            if not facture.categorie:
-                autres, created = Categorie.objects.get_or_create(nom="Autres")
-                facture.categorie = autres
-            facture.save()
-            return redirect('liste_factures')
-    else:
-        form = FactureForm()
-    return render(request, 'factures/creer_facture.html', {'form': form})
+class FactureCreateView(CreateView):
+    model = Facture
+    form_class = FactureForm
+    template_name = 'factures/creer_facture.html'
+    success_url = reverse_lazy('liste_factures')
+
+    def form_valid(self, form):
+        facture = form.save(commit=False)
+        if not facture.categorie:
+            autres, created = Categorie.objects.get_or_create(nom="Autres")
+            facture.categorie = autres
+        facture.save()
+        return super().form_valid(form)
 
 def modifier_facture(request, id):
     facture = get_object_or_404(Facture, id=id)
@@ -35,57 +36,52 @@ def supprimer_facture(request, id):
         return redirect('liste_factures')
     return render(request, 'factures/supprimer_facture.html', {'facture': facture})
 
-def liste_factures(request):
-    client_id = request.GET.get('client_id')
-    categorie_id = request.GET.get('categorie_id')
-    
-    min_montant = request.GET.get('min_montant')
-    max_montant = request.GET.get('max_montant')
+class FactureListView(ListView):
+    model = Facture
+    template_name = 'factures/liste_factures.html'
+    context_object_name = 'factures'
 
-    factures = Facture.objects.all()
-    clients = Client.objects.all()
-    categories = Categorie.objects.all()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['clients'] = Client.objects.all()
+        context['categories'] = Categorie.objects.all()
+        return context
 
-    if client_id:
-        factures = factures.filter(client_id=client_id)
+    def get_queryset(self):
+        factures = Facture.objects.all()
+        client_id = self.request.GET.get('client_id')
+        categorie_id = self.request.GET.get('categorie_id')
+        min_montant = self.request.GET.get('min_montant')
+        max_montant = self.request.GET.get('max_montant')
 
-    if min_montant:
-        factures = factures.filter(montant__gte=min_montant)
-    if max_montant:
-        factures = factures.filter(montant__lte=max_montant)
+        if client_id:
+            factures = factures.filter(client_id=client_id)
+        if min_montant:
+            factures = factures.filter(montant__gte=min_montant)
+        if max_montant:
+            factures = factures.filter(montant__lte=max_montant)
+        if categorie_id:
+            factures = factures.filter(categorie_id=categorie_id)
 
-    if categorie_id:
-        factures = factures.filter(categorie_id=categorie_id)
-
-    return render(request, 'factures/liste_factures.html', {'factures': factures, 'clients': clients, 'categories': categories})
+        return factures
 
 
+class CategorieCreateView(CreateView):
+    model = Categorie
+    form_class = CategorieForm
+    template_name = 'factures/ajouter_categorie.html'
+    success_url = reverse_lazy('liste_factures')
 
-def ajouter_categorie(request):
-    if request.method == "POST":
-        form = CategorieForm(request.POST)
-        if form.is_valid():
-            form.save()  # Sauvegarder la nouvelle catégorie
-            return redirect('liste_factures')  # Redirection après l'ajout, modifie selon ton projet
-    else:
-        form = CategorieForm()
-    
-    return render(request, 'factures/ajouter_categorie.html', {'form': form})
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    template_name = 'factures/ajouter_client.html'
+    success_url = reverse_lazy('liste_factures')
 
-def ajouter_client(request):
-    if request.method == "POST":
-        form = ClientForm(request.POST)
-        if form.is_valid():
-            form.save()  # Sauvegarder le nouveau client
-            return redirect('liste_factures')  # Redirection après l'ajout
-    else:
-        form = ClientForm()
-    
-    return render(request, 'factures/ajouter_client.html', {'form': form})
-
-def liste_clients(request):
-    clients = Client.objects.all()  # Récupérer tous les clients
-    return render(request, 'factures/liste_clients.html', {'clients': clients})
+class ClientList(ListView):
+    model = Client
+    template_name = 'factures/liste_clients.html'
+    context_object_name = 'clients'
 
 
 def index(request):
