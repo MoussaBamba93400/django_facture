666f39d1eb88bd379e027483e0ed1038f27f231e
Moussa Bamba <moussabulls1@gmail.com>
2024-10-24 11:47:41 +0200

    fix precedent commit

--- demo/factures/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Facture, Client  # Assurez-vous que le chemin est correct
-
-class FactureAdmin(admin.ModelAdmin):
-    list_display = ('numero', 'description', 'montant', 'date_creation', 'est_payee', 'client')  # Mettez à jour avec les bons champs
-    search_fields = ('client__nom', 'numero', 'description')  # Champs à rechercher
-
-admin.site.register(Facture, FactureAdmin)
